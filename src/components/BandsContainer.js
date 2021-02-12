3138db190b4de31a980c652db671986b23c33adf
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-02-12 11:12:34 -0700

    Done.

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux'
 class BandsContainer extends Component {
   
   renderBands = () => {
-  return this.props.bands.map((band, id) =>  <li key={id}> {band.bandName}</li>)
+  return this.props.bands.map(band =>  <li key={band.id}> {band.bandName}</li>)
 }
 
   render() {
@@ -14,10 +14,7 @@ class BandsContainer extends Component {
     return (
       <div>
         <BandInput addBand={this.props.addBand}/>
-        <ul>
-             {this.renderBands()}
-        </ul>
-     
+      {this.renderBands()}
       </div>
     )
   }
